23a9196b1c6fd78e1a2bbc956dcaa5a2013f043a
dbstn1325 <dbstn1325@naver.com>
2023-08-11 17:16:40 +0900

    feat: backend Initial Commit

---
+++ backend/src/main/java/com/map/nudonado/NudonadoApplication.java
@@ -0,0 +1,13 @@
+package com.map.nudonado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NudonadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NudonadoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/map/nudonado/NudonadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.nudonado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NudonadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
