338644dfdbdd7935c6c081c434c54f885f2a5977
dbstn1325 <dbstn1325@naver.com>
2023-09-05 21:36:45 +0900

    style: 사용자 자신의 위치 아이콘 그림자 생성

--- frontend/lib/app/ui/android/map/map_page.dart
+++ frontend/lib/app/ui/android/map/map_page.dart
@@ -39,125 +39,131 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      body: Stack(
-          children:[
-            Obx(() {
-              LatLng currentCoord = LatLng(controller.myCoordinate.getLatitude, controller.myCoordinate.getLongitude);
-              print(currentCoord.latitude);
-              return GoogleMap(
-                initialCameraPosition: CameraPosition(target: currentCoord, zoom: 17),
-                myLocationEnabled: false,
-                myLocationButtonEnabled: false,
-                markers: _markers.values.toSet(),
-                onMapCreated: (controller) => _onMapCreated(controller),
-                onTap: (latLng) {
-                  setState(() {
-                    selectedRecords = [];
-                  });
-                },
-              );
-            }),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: InkWell(  // Wrapping the Container with InkWell
-                onTap: () async {
-                  await controller.getMyCoordinate();
-                  Future.delayed(Duration(milliseconds: 500), () {
-                    LatLng newLatLng = LatLng(controller.myCoordinate.getLatitude, controller.myCoordinate.getLongitude);
-                    moveToNewLocation(newLatLng);
-                  });
-                }
-                , // addMarker('test', curLocation),
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 14.0, 180.0),
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40.0),
-                  ),
-                  child: Icon(CupertinoIcons.scope),
-                ),
-              ),
-            ),
-            if (selectedRecords.isNotEmpty)
-              Align(
-                alignment: Alignment.bottomCenter.add(Alignment(0, -0.1)),  // This shifts it up a bit
-                child: Container(
-                  height: 130.0,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: selectedRecords.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        margin: EdgeInsets.only(bottom: 20.0, left: 10.0, right: 10.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(40.0),  // Radius for each Record object
-                          boxShadow: [  // Optional: To give a slight elevation effect
-                            BoxShadow(
-                              color: Colors.black12,
-                              spreadRadius: 0,
-                              blurRadius: 5,
-                              offset: Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("매장 명: ${selectedRecords[index].title}"),
-                            Text("고데기 유무: ${selectedRecords[index].availability}")
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: ElevatedButton(
-                child: Text("마커 고정"),
-                onPressed: () async {
-                  if (mapController != null) {
-                    LatLngBounds bounds = await mapController!.getVisibleRegion();
-                    LatLng centerLatLng = LatLng(
-                      (bounds.northeast.latitude + bounds.southwest.latitude) / 2,
-                      (bounds.northeast.longitude + bounds.southwest.longitude) / 2,
-                    );
-
-                    addMarker('center_marker', centerLatLng);
-                  }
-                },
-              ),
-            ),
-
-            Align(
-              alignment: Alignment.center,
+    return Stack(
+        children:[
+          Obx(() {
+            LatLng currentCoord = LatLng(controller.myCoordinate.getLatitude, controller.myCoordinate.getLongitude);
+            print(currentCoord.latitude);
+            return GoogleMap(
+              initialCameraPosition: CameraPosition(target: currentCoord, zoom: 17),
+              myLocationEnabled: false,
+              myLocationButtonEnabled: false,
+              markers: _markers.values.toSet(),
+              onMapCreated: (controller) => _onMapCreated(controller),
+              onTap: (latLng) {
+                setState(() {
+                  selectedRecords = [];
+                });
+              },
+            );
+          }),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: InkWell(  // Wrapping the Container with InkWell
+              onTap: () async {
+                await controller.getMyCoordinate();
+                Future.delayed(Duration(milliseconds: 500), () {
+                  LatLng newLatLng = LatLng(controller.myCoordinate.getLatitude, controller.myCoordinate.getLongitude);
+                  moveToNewLocation(newLatLng);
+                });
+              }
+              , // addMarker('test', curLocation),
               child: Container(
-                width: 50,
-                height: 50,
-                margin: EdgeInsets.only(bottom: 40),
-                child: Image.asset("assets/images/haruflim_marker.png"),
+                margin: EdgeInsets.fromLTRB(0, 0, 21.0, 30.0),
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(40.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.25), // 그림자의 색상
+                      spreadRadius: 1, // 그림자의 확장 반경
+                      blurRadius: 5, // 그림자의 흐림 반경
+                      offset: Offset(0, 3), // 그림자의 위치
+                    ),
+                  ],
+                ),
+                child: Icon(CupertinoIcons.scope, color: Colors.redAccent,),
               ),
             ),
-
+          ),
+          if (selectedRecords.isNotEmpty)
             Align(
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                  child: Text("부스 생성"),
-                  onPressed: () async {
-                    await boothController.createBooth();
-                  }
-              )
+              alignment: Alignment.bottomCenter.add(Alignment(0, -0.1)),  // This shifts it up a bit
+              child: Container(
+                height: 130.0,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: selectedRecords.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      margin: EdgeInsets.only(bottom: 20.0, left: 10.0, right: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(40.0),  // Radius for each Record object
+                        boxShadow: [  // Optional: To give a slight elevation effect
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 0,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("매장 명: ${selectedRecords[index].title}"),
+                          Text("고데기 유무: ${selectedRecords[index].availability}")
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
+          // Align(
+          //   alignment: Alignment.bottomCenter,
+          //   child: ElevatedButton(
+          //     child: Text("마커 고정"),
+          //     onPressed: () async {
+          //       if (mapController != null) {
+          //         LatLngBounds bounds = await mapController!.getVisibleRegion();
+          //         LatLng centerLatLng = LatLng(
+          //           (bounds.northeast.latitude + bounds.southwest.latitude) / 2,
+          //           (bounds.northeast.longitude + bounds.southwest.longitude) / 2,
+          //         );
+          //
+          //         addMarker('center_marker', centerLatLng);
+          //       }
+          //     },
+          //   ),
+          // ),
+
+          // Align(
+          //   alignment: Alignment.center,
+          //   child: Container(
+          //     width: 50,
+          //     height: 50,
+          //     margin: EdgeInsets.only(bottom: 40),
+          //     child: Image.asset("assets/images/haruflim_marker.png"),
+          //   ),
+          // ),
+
+          // Align(
+          //   alignment: Alignment.center,
+          //   child: ElevatedButton(
+          //       child: Text("부스 생성"),
+          //       onPressed: () async {
+          //         await boothController.createBooth();
+          //       }
+          //   )
+          // ),
 
 
-          ]
-      ),
+        ]
     );
 }
 
