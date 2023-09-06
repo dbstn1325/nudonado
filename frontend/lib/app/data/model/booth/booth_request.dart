9f58326bcb3885b090dd98629b60fb82602a1e5d
dbstn1325 <dbstn1325@naver.com>
2023-09-06 20:18:48 +0900

    feat: 서버로 부스 생성을 요청하기 위한 boothRequest model 생성

--- frontend/lib/app/data/model/booth/booth_request.dart
+++ frontend/lib/app/data/model/booth/booth_request.dart
@@ -1,15 +1,27 @@
 import 'package:frontend/app/data/model/booth/location.dart';
 
 class BoothRequest {
+  final String title;
+  final bool isTimer;
+  final bool isCurlingIron;
+  final String backgroundColorDiversity;
   final String categoryType;
   final Location location;
 
   BoothRequest({
+    required this.title,
+    required this.isTimer,
+    required this.isCurlingIron,
+    required this.backgroundColorDiversity,
     required this.categoryType,
     required this.location,
   });
 
   Map<String, dynamic> toJson() => {
+    'title': title,
+    'isTimer': isTimer,
+    'isCurlingIron': isCurlingIron,
+    'backgroundColorDiversity': backgroundColorDiversity,
     'categoryType': categoryType,
     'location': location.toJson(),
   };
