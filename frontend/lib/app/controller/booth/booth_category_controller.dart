d6f21e6633a405334fa9dd746c9e6dafe30c5fd1
dbstn1325 <dbstn1325@naver.com>
2023-09-06 20:18:03 +0900

    feat: booth의 category model을 getX패턴으로 관리하기 위해 별도 controller 생성

---
+++ frontend/lib/app/controller/booth/booth_category_controller.dart
@@ -0,0 +1,6 @@
+import 'package:frontend/app/data/model/booth/booth_category.dart';
+import 'package:get/get.dart';
+
+class BoothCategoryController extends GetxController {
+  final BoothCategory categoryModel = BoothCategory();
+}
